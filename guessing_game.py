c1cba99a490fb2e8a63f585756ae619072bce5e4
KingBrick <sulwish1293@gmail.com>
2025-10-03 15:41:26 +0100

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -1,5 +1,4 @@
 import random
-
 randomzied_words = [
     "fish",
     "fool",
@@ -7,7 +6,7 @@ randomzied_words = [
     "sum",
     "zack",
     "invader",
-]
+] ## The array that the words are picked from. can be added onto
 
 HANGMANPICS = ['''
   +---+
@@ -61,28 +60,40 @@ HANGMANPICS = ['''
 =========''']
 
 
-randomly_picked = random.choice(randomzied_words)
+randomly_picked = random.choice(randomzied_words) ## picks the random word from the randomized_words table
 
 Total_Tries = 0
 Max_Tries = 6
 
-Hint = randomly_picked[0]
+Hint = randomly_picked[0] ## prints the first letter of the word
 
 
-Reversed_Hint = randomly_picked[::-1]
-last_letter_of_word = Reversed_Hint[0]
+Reversed_Hint = randomly_picked[::-1] ## reverses the word
+last_letter_of_word = Reversed_Hint[0] ## then prints the last letter of the word e.g (hello = olleh == o as the last letter)
 
 print()
 
-World_Length = len(randomly_picked)
-wordBlank = ['_ ' for char in range(World_Length)]
-print("".join(wordBlank))
+World_Length = len(randomly_picked) ## checks the length of the word
+wordBlank = ['_ ' for char in range(World_Length)] ## turns the word into underscores
+print("".join(wordBlank)) ## the word is printed out in blanks when the game starts e.g _ _ _ _
+
+def Comparing_Asnwers(PlayerInput : str, Picked_Word : str, FillingWord : str ): ## comparing player answer to the picked word and seeing if any words are in the same place
+    
+    filling_list = list(FillingWord) ## turns the filling into a list
+
+    for index , char in enumerate(Picked_Word): ## loops the picked_word position
+
+        if char.lower() in PlayerInput.lower(): ## if the user entered a word in the correct spot then it appears
+
+            filling_list[index] = char ## turns the fillingword into a letter e.g ( _ _ _ _ == F _ _ _)
+
+    return "".join(filling_list) ## returns the new list and turns it into a string
 
 while True:
-    PlayerAnswer = str(input("What is the word?"))
+    PlayerAnswer = str(input("What is the word?")) ## asks the player what is the word
 
     if PlayerAnswer == randomly_picked:
-        print("Correct Awnser")
+        print("Correct Awnser the word was : ", randomly_picked)
         print("Game ended")
         break
     else:
@@ -92,31 +103,12 @@ while True:
         print(HANGMANPICS[Total_Tries])
         print()
         print("Wrong")
+        Comparing = Comparing_Asnwers(PlayerAnswer,randomly_picked,wordBlank) ## This part slowly fills out the blanks to show the word
+        print(Comparing)
         print("Tries Left: " , (Max_Tries - Total_Tries))
 
-    if Total_Tries == 3:
-        F_Hint = str(input("Do you want a hint? Y for Yes, N for No")).lower()
-
-        if F_Hint == "y":
-            print("The world starts with an ", Hint)
-        elif F_Hint == "n":
-            print("Ok")
-        else:
-            print("You didnt pick  Y or  N  Final Chance")
-            F_Hint = str(input("Do you want a hint? Y for Yes, N for No")).lower()
-    
-    if Total_Tries == 5:
-        F_Hint = str(input("Do you want a hint? Y for Yes, N for No")).lower()
-
-        if F_Hint == "y":
-            print("The Word Ends with an ", last_letter_of_word)
-        elif F_Hint == "n":
-            print("Ok")
-        else:
-            print("You didnt pick  Y or  N  Final Chance")
-            F_Hint = str(input("Do you want a hint? Y for Yes, N for No")).lower()
-            
-    
     if Total_Tries == Max_Tries:
         print("YOU LOST")
         break
+
+
